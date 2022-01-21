9f98978a17977adc5e749a8e01e2a3816b6d8d52
Daniil Rashin <d.rashin@clicknet.pro>
2022-01-21 17:39:19 +0300

    better tests & final contract i believe...

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -6,6 +6,8 @@ import "@nomiclabs/hardhat-waffle";
 import "@typechain/hardhat";
 import "hardhat-gas-reporter";
 import "solidity-coverage";
+import { ethers } from "ethers";
+import "@nomiclabs/hardhat-web3";
 
 dotenv.config();
 
@@ -19,6 +21,31 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
   for (const account of accounts) {
     console.log(account.address);
   }
+});
+/*
+task("transfer", "Transfers tokens between accounts")
+.addParam("from", "Sender's address")
+.addParam("to", "Receiver's address")
+.setAction(async (taskArgs) => {
+
+});
+
+task("deploy", "Deploys contract to local network and prints its address")
+.setAction(async (hre) => {
+  const token = await hre.ethers.getContractFactory("Croken");
+  const tokenc = await token.deploy(69420);
+  console.log(tokenc.toChecksumAddress());
+});
+*/
+task("balanceOf", "Prints accont balance")
+.addParam("account", "Account to check")
+.addParam("contract", "Contract address")
+.setAction(async (taskArgs, hre) => {
+  const token = await hre.ethers.getContractFactory("Croken");
+  const tokenc = await token.attach(taskArgs.contract);
+  const account = await hre.web3.utils.toChecksumAddress(taskArgs.account);
+  const balance = await tokenc.balanceOf(account);
+  console.log(balance);
 });
 
 // You need to export an object to set up your config
--- test/index.ts
+++ test/index.ts
@@ -1,26 +1,26 @@
 import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
 import { expect } from "chai";
+import { BigNumber } from "ethers";
 import { ethers } from "hardhat";
-import { CheeseKEKToken, CheeseKEKToken__factory } from "../typechain";
+import { Croken, Croken__factory } from "../typechain";
 
-
-describe("Token contract", function () {
-  let Token: CheeseKEKToken;
+describe("Croken contract", function () {
+  let Token: Croken;
   let owner: SignerWithAddress;
   let addr1: SignerWithAddress;
   let addr2: SignerWithAddress;
-  let ownerBalance;
+  let addr3: SignerWithAddress;
+  let ownerBalance: BigNumber;
 
   before(async function () {
-    [owner, addr1, addr2] = await ethers.getSigners();
+    [owner, addr1, addr2, addr3] = await ethers.getSigners();
 
-    const chkkTokenFactory = (await ethers.getContractFactory(
-      "CheeseKEKToken", owner
-      )) as CheeseKEKToken__factory;
+    const crokenFactory = (await ethers.getContractFactory(
+      "Croken", owner
+      )) as Croken__factory;
 
-    Token = await chkkTokenFactory.deploy(69420);
+    Token = await crokenFactory.deploy(ethers.utils.parseEther("0"));
   });
-
 
   describe("Deployment", function () {
     it("Should assign the total supply of tokens to the owner", async function () {
@@ -29,41 +29,60 @@ describe("Token contract", function () {
     });
   });
 
-  describe("Transactions", function () {
-		it("Should transfer tokens between accounts", async function () {
-        await Token.transfer(addr1.address, 1337);
-        const addr1Balance = await Token.balanceOf(addr1.address);
-        expect(addr1Balance).to.equal(1337);
+  describe("Minting/burning", function () {
+    const baseAmount = ethers.utils.parseEther("138840");
+    const newAmount = ethers.utils.parseEther("69420");
 
-        await Token.connect(addr1).transfer(addr2.address, 322);
+    it("Owner mints some tokens", async function () {
+      await Token._mint(owner.address, baseAmount);
+      expect (
+        await Token.balanceOf(owner.address)
+      ).to.be.equal(baseAmount);
+    });
+
+    it("Owner burns some tokens", async function () {
+      await Token._burn(owner.address, newAmount);
+      expect (
+        await Token.balanceOf(owner.address)
+      ).to.be.equal(newAmount);
+    });
+
+    it("Others can't mint/burn tokens", async function () {
+      await expect (
+        Token.connect(addr1)._mint(addr2.address, newAmount)
+      ).to.be.revertedWith("You are not the owner!");
+      await expect (
+         Token.connect(addr2)._burn(owner.address, newAmount)
+      ).to.be.revertedWith("You are not the owner!");
+    });
+  });
+
+  describe("Transactions", function () {
+    const balance1 = ethers.utils.parseEther("1337");
+    const balance2 = ethers.utils.parseEther("322");
+
+		it("Should transfer tokens between accounts & update balances", async function () {
+        await Token.transfer(addr1.address, balance1);
+        const addr1Balance = await Token.balanceOf(addr1.address);
+        expect(addr1Balance).to.equal(balance1);
+
+        await Token.connect(addr1).transfer(addr2.address, balance2);
         const addr2Balance = await Token.balanceOf(addr2.address);
-        expect(addr2Balance).to.equal(322);
+        expect(addr2Balance).to.equal(balance2);
+
+        await Token.transfer(addr3.address, balance1.add(balance2));
+        const addr3Balance = await Token.balanceOf(addr3.address);
+        expect(addr3Balance).to.equal(balance1.add(balance2));
     });
 
     it("Should fail if sender doesn’t have enough tokens", async function () {
         const initialOwnerBalance = await Token.balanceOf(owner.address);
 
         await expect (
-          Token.connect(addr2).transfer(owner.address, 69420)
-        ).to.be.revertedWith("ERC20: transfer amount exceeds balance");
+          Token.connect(addr2).transfer(owner.address, initialOwnerBalance)
+        ).to.be.revertedWith("Insufficient balance");
 
         expect(await Token.balanceOf(owner.address)).to.equal(initialOwnerBalance);
-    });
-
-    it("Should update balances after transfers", async function () {
-        const initialOwnerBalance = await Token.balanceOf(owner.address);
-
-        await Token.transfer(addr1.address, 100);
-        await Token.transfer(addr2.address, 50);
-
-        const finalOwnerBalance = await Token.balanceOf(owner.address);
-        expect(finalOwnerBalance).to.equal(initialOwnerBalance.sub(150));
-
-        const addr1Balance = await Token.balanceOf(addr1.address);
-        expect(addr1Balance).to.be.equal(1115);
-
-        const addr2Balance = await Token.balanceOf(addr2.address);
-        expect(addr2Balance).to.be.equal(372);
     });
   });
 });
